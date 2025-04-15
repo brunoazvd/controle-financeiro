74f301087eb3f3fb6075fc453b0240e825dc419d
Bruno Azevedo <brunoazvd@protonmail.com>
2025-04-15 17:24:34 -0300

    crude user authentication

--- client/src/general/Router.jsx
+++ client/src/general/Router.jsx
@@ -1,9 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 
+import { Layout } from "./Layout.jsx";
+
 import { RoutePaths } from "../static/RoutePaths.js";
+
 import { Home } from "../pages/Home.jsx";
 import { NotFound } from "../pages/NotFound.jsx";
-import { Layout } from "./Layout.jsx";
+import { Login } from "../pages/Login.jsx";
+import { Register } from "../pages/Register.jsx";
 
 export const Router = () => (
   <Routes>
@@ -12,6 +16,22 @@ export const Router = () => (
       element={
         <Layout>
           <Home />
+        </Layout>
+      }
+    />
+    <Route
+      path={"/login"}
+      element={
+        <Layout>
+          <Login />
+        </Layout>
+      }
+    />
+    <Route
+      path={"/register"}
+      element={
+        <Layout>
+          <Register />
         </Layout>
       }
     />
---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import { authRouter } from './auth.js';
+
+const port = 3005;
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/auth", authRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
